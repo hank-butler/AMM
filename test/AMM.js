36170b251df27680e0bda42423a3992ca9074cbc
Hank <hank.butler@protonmail.com>
2023-12-14 20:05:33 -0700

    constructor for sol file and associated tests

--- test/AMM.js
+++ test/AMM.js
@@ -5,7 +5,7 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-const ether = token;
+const ether = tokens;
 
 describe('AMM', () => {
     let accounts, deployer, token1, token2, amm
@@ -19,6 +19,21 @@ describe('AMM', () => {
         token2 = await Token.deploy('USD Token', 'USD', '1000000')
 
         const AMM = await ethers.getContractFactory('AMM')
-        amm = await AMM.deploy()
+        amm = await AMM.deploy(token1.address, token2.address)
+
+    })
+
+    describe('Deployment', () => {
+        it('has an address', async () => {
+            expect(amm.address).to.not.equal(0x0)
+        })
+
+        it('returns token 1', async () => {
+            expect(await amm.token1()).to.equal(token1.address)
+        })
+
+        it('returns token 2', async () => {
+            expect(await amm.token2()).to.equal(token2.address)
+        })
     })
 })
