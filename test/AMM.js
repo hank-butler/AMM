90d57acb71efc9837ad1534d9beae4b478472bbe
Hank <hank.butler@protonmail.com>
2023-12-28 14:55:18 -0700

    adding liquidity function and tests

--- test/AMM.js
+++ test/AMM.js
@@ -8,16 +8,34 @@ const tokens = (n) => {
 const ether = tokens;
 
 describe('AMM', () => {
-    let accounts, deployer, token1, token2, amm
+    // accounts up here
+    let accounts, 
+        deployer,
+        liquidityProvider 
+    // contracts 
+    let token1, 
+        token2, 
+        amm
 
     beforeEach(async () => {
+        // Setup Accounts
         accounts = await ethers.getSigners()
         deployer = accounts[0]
+        liquidityProvider = accounts[1]
 
+        // Deploy Token
         const Token = await ethers.getContractFactory('Token')
         token1 = await Token.deploy('Dapp University', 'DAPP', '1000000')
         token2 = await Token.deploy('USD Token', 'USD', '1000000')
 
+        // Send tokens to litquidity provider
+        let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+        await transaction.wait()
+        
+        transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+        await transaction.wait()
+
+        // Deploy AMM
         const AMM = await ethers.getContractFactory('AMM')
         amm = await AMM.deploy(token1.address, token2.address)
 
@@ -34,6 +52,30 @@ describe('AMM', () => {
 
         it('returns token 2', async () => {
             expect(await amm.token2()).to.equal(token2.address)
+        })
+    })
+
+    describe('Swapping Tokens', () => {
+
+        let amount, transaction, result 
+
+        it('facilitates swaps', async () => {
+            amount = tokens(100000)
+            transaction = await token1.connect(deployer).approve(amm.address, amount)
+            await transaction.wait()
+
+            transaction = await token2.connect(deployer).apporve(amm.address, amount)
+            await transaction.wait()
+
+            // Deployer add liquidity
+            transaction = await amm.connect(deployer).addLiquidity(amount, amount)
+            await transaction.wait()
+
+            // Check AMM receives tokens
+            expect(await token1.balanceOf(amm.address)).to.equal(amount)
+            expect(await token2.balanceOf(amm.address)).to.equal(amount)
+
+            
         })
     })
 })
